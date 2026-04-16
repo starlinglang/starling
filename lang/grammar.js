226cdc467ca90e7bc8fb848cdaea0dda551326b8
lelouch <hi@enderverse.world>
2026-04-16 16:43:14 -0400

    modifying grammar

--- lang/grammar.js
+++ lang/grammar.js
@@ -1,36 +1,39 @@
 const ohm = require("ohm-js");
 
 const starlingGrammar = String.raw`
-  Starling {
-       Database = Outermost_scope_stmt+
-       Outermost_scope_stmt = import_stmt | Const | Disjoint | To_sub | Proof_block | Block | comment
-       Block_content = Disjoint | Block | Block_to_sub | comment
-       Block = "block"  "{" Block_content+  "}"
-       Block_to_sub = math_symbol  "="  Block_inner
-       Block_inner = Variable | Axiom | Essential_hyp
-       Proof_block = "proof of " math_symbol "{"  Proof_unit+  "}"
-       Proof_unit = comment | Proof_cell
-       Proof_cell = math_symbol  ";"
-       To_sub = math_symbol  "="  Inner
-       Inner = Variable | Axiom | Theorem | Essential_hyp
-       Essential_hyp = "assume" NonemptyListOf<math_symbol, ""> ":"  math_symbol ";"
-       Theorem =  NonemptyListOf<math_symbol, "">  ":"   math_symbol ";"
-	     Axiom = "axiom" math_symbol  ":"  math_symbol  ";"
-       Variable = "fix" VariableListItem  ";"
-       VariableListItem = math_symbol  ":" math_symbol
-       Disjoint = "distinct" NonemptyListOf<math_symbol, ","> ";"
+Starling {
+        Database = Outermost_scope_stmt+
+        Outermost_scope_stmt = import_stmt | Const | Replace | Disjoint | To_sub | Proof_block | Block | comment
+        Block_content = Disjoint | Block | Block_to_sub | comment
+        Block = "block"  "{" Block_content+  "}"
+        Block_to_sub = math_symbol  "="  Block_inner
+        Block_inner = Variable | Axiom | Essential_hyp
+        Proof_block = "proof of" math_symbol "{"  Proof_unit+  "}"
+        Proof_unit = comment | Proof_cell
+        Proof_cell = math_symbol  ";"
+        To_sub = math_symbol  "="  Inner
+        Inner = Variable | Axiom | Theorem | Essential_hyp
+        Essential_hyp = "assume" NonemptyListOf<math_symbol, ""> ":"  math_symbol ";"
+        Theorem =  NonemptyListOf<math_symbol, "">  ":"   math_symbol ";"
+	    Axiom = "axiom" math_symbol  ":"  math_symbol  ";"
+	   Replace = "replace" "{" NonemptyListOf<ReplaceListItem, ","> "}" ";"
+        ReplaceListItem =  replaceCharacters ":" math_symbol
+        Variable = "fix" VariableListItem  ";"
+        VariableListItem = math_symbol  ":" math_symbol
+        Disjoint = "distinct" NonemptyListOf<math_symbol, ","> ";"
 	     Const = "define" NonemptyListOf<math_symbol, ","> ";"
-       import_stmt = "import \"" any_printable_ascii_except_dollar_newline+ "\"" import_alias? ";"
-       import_alias = " as " math_symbol
-     	 math_symbol = const_symbol+
-       any_printable_ascii_except_dollar_newline =  "!"  | "#" | "%" | "&" | "'" | "(" | ")" | "*" | "+" | "," | "-" | "." |
-         "/" | alnum | ":" | "<" | ">" | "?" | "@"  | "{" | "|" | "}" | "~"
-       const_symbol = "!" | "#" | "%" | "&" | "*" | "+" | "-" | "." | "(" | ")" |  "/" | alnum  | "<" | ">" | "?" | "@"  | "|" | "~"
-       comment = multiLineComment | singleLineComment
-	     sourceCharacter = any
-       lineTerminator = "\n" | "\r" | "\u2028" | "\u2029"
-       multiLineComment = "/*" (~"*/" sourceCharacter)* "*/"
-       singleLineComment = "//" (~lineTerminator sourceCharacter)*
-   }`;
+        import_stmt = "import" "\"" any_printable_ascii_except_dollar_newline+ ";"
+      	 math_symbol = const_symbol+
+        replaceCharacters = replaceCharacter+
+        replaceCharacter = any_printable_ascii_except_dollar_newline | "="
+        any_printable_ascii_except_dollar_newline =  "!"  | "#" | "%" | "&" | "'" | "(" | ")" | "*" | "+" | "," | "-" | "." |
+          "/" | alnum | ":" | "<" | ">" | "?" | "@"  | "{" | "|" | "}" | "~"
+        const_symbol = "!" | "#" | "%" | "&" | "*" | "+" | "-" | "." | "(" | ")" |  "/" | alnum  | "<" | ">" | "?" | "@"  | "|" | "~"
+        comment = multiLineComment | singleLineComment
+        lineTerminator = "\n" | "\r" | "\u2028" | "\u2029"
+        multiLineComment = "/*" (~"*/" sourceCharacter)* "*/"
+        singleLineComment = "//" (~lineTerminator sourceCharacter)*
+        sourceCharacter = any
+}`;
 
 module.exports = { starlingGrammar };
--- lang/tests/testing.js
+++ lang/tests/testing.js
@@ -65,20 +65,22 @@ let longTest = `
 
 //shortTest.star
 let shortTest = `
-  define <, >, implies, formula;
+  define (, );
+  replace {= : equals,  wff : formula};
+
   wp = fix p: formula;
   wq = fix q: formula;
   wr = fix r: formula;
   ws = fix s: formula;
-  w2 = axiom < p implies q >:formula;
-  wnew = < s implies < r implies p > >:formula;
+  w2 = axiom ( p implies q ):formula;
+  wnew = ( s implies ( r implies p ) ):formula;
 
   proof of wnew {
-    ws;
-    wr;
-    wp;
-    w2;
-    w2;
+      ws;
+      wr;
+      wp;
+      w2;
+      w2;
   }
   `;
 
